4b9fbb85f22351cfa88ba57bb6c63307be1972a8
Elad P <eladpar@gmail.com>
2023-08-22 14:39:14 +0300

    aded zed2i

--- launch/zed_rgb_convert.launch.py
+++ launch/zed_rgb_convert.launch.py
@@ -1,17 +1,3 @@
-# Copyright 2023 Stereolabs
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 
 from launch import LaunchDescription
@@ -28,10 +14,10 @@ def generate_launch_description():
     #  - 'zed' for "ZED" camera
     #  - 'zedm' for "ZED mini" camera
     #  - 'zed2' for "ZED2" camera
-    camera_model = 'zed2'
+    camera_model = 'zed2i'
 
     # Camera name
-    camera_name = 'zed2'
+    camera_name = 'zed2i'
 
     # URDF/xacro file to be loaded by the Robot State Publisher node
     xacro_path = os.path.join(
